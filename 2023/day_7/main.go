5596462e69b80e6437f86e82024263c78d8e0c18
Sergey <heyyyoyy@gmail.com>
2023-12-09 18:58:26 +0300

    day7 part 2

--- 2023/day_7/main.go
+++ 2023/day_7/main.go
@@ -87,7 +87,7 @@ func parse1(input string) []Card {
 			}
 		}
 
-		values := make([]string, 0, len(handBid[0]))
+		values := make([]string, 0, len(handMap))
 		for _, v := range handMap {
 			values = append(values, strconv.Itoa(v))
 		}
@@ -124,8 +124,122 @@ func parse1(input string) []Card {
 	return cards
 }
 
-func part1(input string) int {
-	cards := parse1(input)
+func getCardTypeStr(handMap map[cardLabel]int, card cardLabel) string {
+	jockerCount, contains := handMap[card]
+	if contains {
+		if jockerCount == 5 {
+			return "5"
+		}
+		values := make([]int, 0, len(handMap))
+		for k, v := range handMap {
+			if k == card {
+				continue
+			} 
+			values = append(values, v)
+		}
+		slices.Sort(values)
+		values[len(values) - 1] += jockerCount
+		valuesStr := make([]string, 0, len(values))
+		for _, v := range values {
+			valuesStr = append(valuesStr, strconv.Itoa(v))
+		}
+		return strings.Join(valuesStr, "")
+	}
+	values := make([]string, 0, len(handMap))
+	for _, v := range handMap {
+		values = append(values, strconv.Itoa(v))
+	}
+	slices.Sort(values)
+	return strings.Join(values, "")
+}
+
+func parse2(input string) []Card {
+	const (
+		Jocker cardLabel = iota + 1
+		Two
+		Three
+		Four
+		Five
+		Six
+		Seven
+		Eight
+		Nine
+		Ten
+		Queen
+		King
+		Ace
+	)
+	lines := strings.Split(input, "\n")
+	cards := make([]Card, 0, len(lines))
+	for _, line := range lines {
+		handBid := strings.Split(line, " ")
+		bid, err := strconv.Atoi(handBid[1])
+		if err != nil {
+			log.Fatalf("Wrong bid: %s", handBid[1])
+		}
+
+		hand := make([]cardLabel, 0, len(handBid[0]))
+		handMap := make(map[cardLabel]int, len(handBid[0]))
+
+		for _, ch := range handBid[0] {
+			switch ch {
+			case 'T':
+				hand = append(hand, Ten)
+				handMap[Ten]++
+			case 'J':
+				hand = append(hand, Jocker)
+				handMap[Jocker]++
+			case 'Q':
+				hand = append(hand, Queen)
+				handMap[Queen]++
+			case 'K':
+				hand = append(hand, King)
+				handMap[King]++
+			case 'A':
+				hand = append(hand, Ace)
+				handMap[Ace]++
+			default:
+				num := cardLabel(ch - '0')
+				if num < 2 || num > 9 {
+					log.Fatalf("Wrong card in hand: %v", ch)
+				}
+				hand = append(hand, num)
+				handMap[num]++
+			}
+		}
+		typeStr := getCardTypeStr(handMap, Jocker)
+
+		var cardType cardType
+		switch typeStr {
+		case "5":
+			cardType = FiveOfAKind
+		case "14":
+			cardType = FourOfAKind
+		case "23":
+			cardType = FullHouse
+		case "113":
+			cardType = ThreeOfAKind
+		case "122":
+			cardType = TwoPair
+		case "1112":
+			cardType = OnePair
+		case "11111":
+			cardType = HighCard
+		default:
+			log.Fatalf("Wrong type card: %s", typeStr)
+		}
+
+		card := Card{
+			bid: bid,
+			hand: hand,
+			cardType: cardType,
+		}
+		cards = append(cards, card)
+	}
+	return cards
+}
+
+func getRes(cards []Card) int {
 	sort.Slice(cards, func(i, j int) bool {
 		if cards[i].cardType != cards[j].cardType {
 			return cards[i].cardType < cards[j].cardType
@@ -148,6 +262,15 @@ func part1(input string) int {
 	}
 	return total
 }
+func part1(input string) int {
+	cards := parse1(input)
+	return getRes(cards)
+}
+
+func part2(input string) int {
+	cards := parse2(input)
+	return getRes(cards)
+}
 
 func main() {
 	data, err := os.ReadFile("input.txt")
@@ -156,4 +279,5 @@ func main() {
 	}
 	input := string(data)
 	fmt.Println(part1(input))
+	fmt.Println(part2(input))
 }
